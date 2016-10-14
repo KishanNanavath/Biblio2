b0babe5dccd9619780822887c334d9e6bb64e19f
KishanNanavath <nbalkishan@gmail.com>
2016-10-14 21:46:34 +0530

    updated

--- app/src/main/java/com/example/kishan/biblio/GenrePage.java
+++ app/src/main/java/com/example/kishan/biblio/GenrePage.java
@@ -21,13 +21,24 @@ import java.util.ArrayList;
 public class GenrePage extends Fragment {
 
     RecyclerView recFiction;
-    RecyclerView recRomance;
-    CategoryAdapter adapter;
-    CategoryAdapter adapter1;
+    CategoryAdapter adapterFiction;
     ArrayList<BooksGetter> genreBooks;
-    ArrayList<BooksGetter> romanceBooks;
     SwipyRefreshLayout srl;
+
+    RecyclerView recRomance;
+    CategoryAdapter adapterRomance;
+    ArrayList<BooksGetter> romanceBooks;
     SwipyRefreshLayout srlRomance;
+
+    RecyclerView recAdventure;
+    CategoryAdapter adapterAdventure;
+    ArrayList<BooksGetter> AdventureBooks;
+    SwipyRefreshLayout srlAdventure;
+
+    RecyclerView recBiography;
+    CategoryAdapter adapterBiography;
+    ArrayList<BooksGetter> BiographyBooks;
+    SwipyRefreshLayout srlBiography;
 
     final String url = "https://www.googleapis.com/books/v1/volumes?q=";
 
@@ -40,31 +51,61 @@ public class GenrePage extends Fragment {
         srl = (SwipyRefreshLayout) view.findViewById(R.id.srlFiction);
         genreBooks = new ArrayList<>();
         recFiction = (RecyclerView)view.findViewById(R.id.rvFiction);
-        adapter = new CategoryAdapter(genreBooks,getContext());
+        adapterFiction = new CategoryAdapter(genreBooks,getContext());
 
         try {
-            new BooksAsyncTask(getActivity(),genreBooks,adapter,recFiction).execute(url+ URLEncoder.encode("subject:\"Fiction\"","UTF-8")+"&maxResults=40&printType=books&orderBy=newest",0,srl);
+            new BooksAsyncTask(getActivity(),genreBooks,adapterFiction,recFiction).execute(url+ URLEncoder.encode("subject:\"Fiction\"","UTF-8")+"&maxResults=40&printType=books&orderBy=newest",0,srl);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
-        recFiction.setAdapter(adapter);
+        recFiction.setAdapter(adapterFiction);
         recFiction.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
 
         //************************************************************8
         srlRomance = (SwipyRefreshLayout) view.findViewById(R.id.srlRomance);
         romanceBooks = new ArrayList<>();
         recRomance = (RecyclerView)view.findViewById(R.id.rvRomance);
-        adapter1 = new CategoryAdapter(romanceBooks,getContext());
+        adapterRomance = new CategoryAdapter(romanceBooks,getContext());
 
         try {
-            new BooksAsyncTask(getActivity(),romanceBooks,adapter1,recRomance).execute(url+ URLEncoder.encode("subject:\"Adventure\"","UTF-8")+"&maxResults=40&printType=books&orderBy=newest",0,srl);
+            new BooksAsyncTask(getActivity(),romanceBooks,adapterRomance,recRomance).execute(url+ URLEncoder.encode("subject:\"Romance\"","UTF-8")+"&maxResults=40&printType=books&orderBy=newest",0,srl);
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
 
-        recRomance.setAdapter(adapter1);
+        recRomance.setAdapter(adapterRomance);
         recRomance.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+
+        //************************************************************8
+        srlAdventure = (SwipyRefreshLayout) view.findViewById(R.id.srlAdventure);
+        AdventureBooks = new ArrayList<>();
+        recAdventure = (RecyclerView)view.findViewById(R.id.rvAdventure);
+        adapterAdventure = new CategoryAdapter(AdventureBooks,getContext());
+
+        try {
+            new BooksAsyncTask(getActivity(),AdventureBooks,adapterAdventure,recAdventure).execute(url+ URLEncoder.encode("subject:\"Adventure\"","UTF-8")+"&maxResults=40&printType=books&orderBy=newest",0,srl);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        recAdventure.setAdapter(adapterAdventure);
+        recAdventure.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
+
+        //************************************************************8
+        srlBiography = (SwipyRefreshLayout) view.findViewById(R.id.srlBiography);
+        BiographyBooks = new ArrayList<>();
+        recBiography = (RecyclerView)view.findViewById(R.id.rvBiography);
+        adapterBiography = new CategoryAdapter(BiographyBooks,getContext());
+
+        try {
+            new BooksAsyncTask(getActivity(),BiographyBooks,adapterBiography,recBiography).execute(url+ URLEncoder.encode("subject:\"Biography\"","UTF-8")+"&maxResults=40&printType=books&orderBy=newest",0,srl);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        recBiography.setAdapter(adapterBiography);
+        recBiography.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL,false));
 
         return view;
     }
